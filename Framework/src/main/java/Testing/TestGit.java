712f9c155b911a7a94603783a8c838c3935b0bd2
Gokul.UdayaKumar <Gokul.UdayaKumar@IN23GELML253YHG.na.wkglobal.com>
2021-08-02 22:47:08 +0530

    Commiting test to branchX

---
+++ Framework/src/main/java/Testing/TestGit.java
@@ -0,0 +1,13 @@
+package Testing;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("This is a GIT Test");
+	}
+
+}
